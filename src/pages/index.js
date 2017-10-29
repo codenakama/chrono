98a5735acb830d098ec0268a93bcaa5c60005933
Ricardo Abreu <rj.abreu89@gmail.com>
2017-10-29 13:09:54 +0000

    cleanup materiaul ui example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,12 @@
-/* eslint-disable flowtype/require-valid-file-annotation */
-
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import Button from "material-ui/Button";
-import Dialog, {
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  DialogActions
-} from "material-ui/Dialog";
 import Typography from "material-ui/Typography";
 import AppBar from "material-ui/AppBar";
 import Toolbar from "material-ui/Toolbar";
-
 import { withStyles } from "material-ui/styles";
 import withRoot from "../components/withRoot";
 
 class Index extends Component {
-  state = {
-    open: false
-  };
-
-  handleRequestClose = () => {
-    this.setState({
-      open: false
-    });
-  };
-
-  handleClick = () => {
-    this.setState({
-      open: true
-    });
-  };
-
   render() {
     return (
       <div className={this.props.classes.root}>
