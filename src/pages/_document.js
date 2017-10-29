7082fadedde9e87e2ba78d066a94af5703ea6de7
Ricardo Abreu <rj.abreu89@gmail.com>
2017-10-29 13:08:40 +0000

    added material ui

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,22 +1,97 @@
 import Document, { Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+import { injectGlobal } from "styled-components";
+import { ThemeProvider } from "styled-components";
+import theme from "../theme/theme";
+import JssProvider from "react-jss/lib/JssProvider";
+import getContext from "../styles/getContext";
 
-export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    const sheet = new ServerStyleSheet();
-    const page = renderPage(App => props =>
-      sheet.collectStyles(<App {...props} />)
-    );
-    const styleTags = sheet.getStyleElement();
-    return { ...page, styleTags };
-  }
+injectGlobal`
+@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
 
+/* reset css */
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+
+body {
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+    background-color: #F6F7F7;  
+}
+`;
+
+class MyDocument extends Document {
   render() {
     return (
-      <html>
+      <html lang="en" dir="ltr">
         <Head>
-          <title>Chrono</title>
-          {this.props.styleTags}
+          <title>My page</title>
+          <meta charSet="utf-8" />
+          {/* Use minimum-scale=1 to enable GPU rasterization */}
+          <meta
+            name="viewport"
+            content={
+              "user-scalable=0, initial-scale=1, " +
+              "minimum-scale=1, width=device-width, height=device-height"
+            }
+          />
+          {/*
+            manifest.json provides metadata used when your web app is added to the
+            homescreen on Android. See https://developers.google.com/web/fundamentals/engage-and-retain/web-app-manifest/
+          */}
+          <link rel="manifest" href="/static/manifest.json" />
+          {/* PWA primary color */}
+          <meta
+            name="theme-color"
+            content={this.props.stylesContext.theme.palette.primary[500]}
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+          />
         </Head>
         <body>
           <Main />
@@ -26,3 +101,44 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+MyDocument.getInitialProps = ctx => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. page.getInitialProps
+  // 2. document.getInitialProps
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the server with error:
+  // 2. document.getInitialProps
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. page.getInitialProps
+  // 3. page.render
+
+  // Get the context to collected side effects.
+  const context = getContext();
+  const page = ctx.renderPage(Component => props => (
+    <JssProvider registry={context.sheetsRegistry} jss={context.jss}>
+      <Component {...props} />
+    </JssProvider>
+  ));
+
+  return {
+    ...page,
+    stylesContext: context,
+    styles: (
+      <style
+        id="jss-server-side"
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: context.sheetsRegistry.toString() }}
+      />
+    )
+  };
+};
+
+export default MyDocument;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,55 @@
-import styled from "styled-components";
+/* eslint-disable flowtype/require-valid-file-annotation */
 
-const Wrapper = styled.div`background-color: red;`;
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Button from "material-ui/Button";
+import Dialog, {
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions
+} from "material-ui/Dialog";
+import Typography from "material-ui/Typography";
+import AppBar from "material-ui/AppBar";
+import Toolbar from "material-ui/Toolbar";
 
-export default () => <Wrapper>hello world</Wrapper>;
+import { withStyles } from "material-ui/styles";
+import withRoot from "../components/withRoot";
+
+class Index extends Component {
+  state = {
+    open: false
+  };
+
+  handleRequestClose = () => {
+    this.setState({
+      open: false
+    });
+  };
+
+  handleClick = () => {
+    this.setState({
+      open: true
+    });
+  };
+
+  render() {
+    return (
+      <div className={this.props.classes.root}>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography type="title" color="inherit">
+              Chrono
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+Index.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withRoot(withStyles()(Index));
