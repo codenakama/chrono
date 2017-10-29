5ebac809aa567795b25c076f3637d1d137ecf600
Ricardo Abreu <rj.abreu89@gmail.com>
2017-10-29 23:41:38 +0000

    timeline page and timeline card temp image

--- src/components/TimelineCard.js
+++ src/components/TimelineCard.js
@@ -9,7 +9,7 @@ class TimelineCard extends Component {
     const { title, description, views, category } = this.props;
     return (
       <Card href="http://google.com">
-        <Image src="/assets/images/avatar/large/matthew.png" />
+        <Image src="https://images.unsplash.com/photo-1493119508027-2b584f234d6c?dpr=2&auto=format&fit=crop&w=568&h=568&q=60&cs=tinysrgb&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" />
         <Card.Content>
           {category && (
             <StyledCardMeta>
