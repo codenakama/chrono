9e66e569003ea22d3f295386e4a0f5960ced996a
Ricardo Abreu <rj.abreu89@gmail.com>
2017-10-29 23:59:27 +0000

    create timeline page

---
+++ src/pages/create.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import TimelinesList from "../components/TimelinesList";
+import { Container } from "semantic-ui-react";
+import { Header } from "semantic-ui-react";
+import Layout from "../components/Layout";
+import { Grid, Button } from "semantic-ui-react";
+
+export default () => <Layout>create</Layout>;
